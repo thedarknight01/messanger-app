68101bde66eec05eda85e43c4d6961d4388e0f3e
Nitin Tiwari <95606924+thedarknight01@users.noreply.github.com>
2024-10-05 12:14:54 +0530

    Create page.tsx

---
+++ app/(site)/Message/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import AuthForm from './components/AuthForm';
+export default function () {
+    return (
+        <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                < Image alt="Logo" height={48} width={48} className='mx-auto w-auto' src="/images/logo.png"/>
+
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Messanger 
+                </h2>
+            </div>
+            <AuthForm/>
+
+        </div>
+    );
+  }
+  
